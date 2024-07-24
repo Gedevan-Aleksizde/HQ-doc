5ef39960dad6a7c9b486d4dbf6991b1e37e8b1f5
Satoshi Katagiri <katagiri.stsh@gmail.com>
2024-07-25 00:05:12 +0900

    wip

--- script/common.R
+++ script/common.R
@@ -5,9 +5,13 @@ d <- data.frame(
   icon_num = factor(1:4, levels = 1:5),
   xbox = c("X", "Y", "A", "B"),
   key = c("T", "Y", "G", "H"),
-  ps = c("□",  "△", "❌",  "○"),
-  x = c(0, 1, 0, 1),
-  y = c(1, 1, 0, 0)
+  ps = c("□", 
+         "△",
+         "×",
+         "○"
+  ),
+  x = c(-1, 1, -1, 1) / (300 * .pt),
+  y = c(1, 1, -1, -1) / (300 * .pt)
   #x = c(0, 0.5, 0.5, 1),
   #y = c(0, sqrt(2)/2, -sqrt(2)/2, 0)
 )
@@ -27,18 +31,43 @@ ragg_png_mod <- function(...){
   ragg::agg_png(..., units = "in", res = 300)
 }
 
-make_buttons <- function(buttons, type = "xbox", dev = ragg_png_mod, save = T, dir = NULL, name = paste0(paste(buttons, collapse = "+"), ".png")){
-  g <- d %>% mutate(!!sym(type) := if_else(key %in% buttons, !!sym(type), NA_character_)) %>%
-    ggplot(aes(x = x, y = y)) +
-    geom_point(
-      aes(color = "black", fill = factor(if_else(!is.na(!!sym(type)), as.integer(icon_num), as.integer(5)), 1:5)),
-      size = 15 * .pt, shape = 21, color = "black") +
-    geom_text(aes(label = !!sym(type)), size = 10 * .pt) +
-    coord_fixed(xlim = c(0 -1/.pt, 1 + 1/.pt), ylim = c(0, 1)) +
+make_buttons_svg <- function(buttons, type = "xbox", dev = svglite::svglite, save = T, dir = NULL, name = paste0(paste(buttons, collapse = "+"), ".svg")){
+  if(type == "xbox"){
+    g <- d %>% mutate(!!sym(type) := if_else(key %in% buttons, !!sym(type), NA_character_)) %>%
+      ggplot(aes(x = x, y = y)) +
+      geom_point(
+        aes(fill = factor(if_else(!is.na(!!sym(type)), as.integer(icon_num), as.integer(5)), 1:5)),
+        size = 20/ .pt, shape = 21, color = "black"
+      ) +
+      geom_text(
+        aes(
+          label = !!sym(type)
+        ),
+        size = 15 / .pt)
+  } else {
+    g <- d %>% mutate(
+      !!sym(type) := if_else(key %in% buttons, !!sym(type), NA_character_),
+      x = if(type == "key") if_else(y > 0, x - .15 * abs(x), x + .15 * abs(x)) else  x
+    ) %>%
+      ggplot(aes(x = x, y = y)) +
+      geom_point(
+        size = 20/ .pt, shape = ifelse(type == "key", 22, 21), color = "black",
+        fill = "grey"
+      ) +
+      geom_text(
+        aes(
+          label = !!sym(type),
+          color = factor(if_else(!is.na(!!sym(type)), as.integer(icon_num), as.integer(5)), 1:5)
+        ),
+        size = 15 / .pt)
+  }
+  g <- g +
+    coord_fixed(xlim = c(-1, 1)/ (150 * .pt), ylim = c(-1, 1)/(150 * .pt)) +
     scale_fill_manual(guide = F, breaks = factor(1:5), values = c("blue", "yellow", "green", "red", "grey")) +
-    theme_void()
+    scale_color_manual(guide = F, breaks = factor(1:5), values = c("blue", "yellow", "green", "red", "grey")) +
+    theme_void(base_family = "Noto Sans CJK JP")
   if(save){
-    ggsave(plot = g, device = dev, path = dir, filename = name)
+    ggsave(plot = g, device = dev, path = dir, filename = name, width = 1, height = 1, scale = .5)
   } else {
     return(g)
   }
@@ -66,16 +95,16 @@ make_direct_buttons <- function(
 }
 
 reuse_svg <- function(id){
-  sprintf('<img height=40 width=40 href="%s" />', id)
+  sprintf('<img height=40 width=40 src="%s" />', id)
 }
 
-replace_keys <- function(text){
+replace_keys <- function(text, dir = "../../../../img/xbox"){
   text %>% str_remove_all("\\s") %>%
-    str_match_all("\\[([TYGHRLUD\\+]+?)\\]|[^\\[([TYGHRLUD\\+]+?)\\]]+") %>%
+    str_match_all("\\[([TYGHRLUD]+?)\\]|[^\\[([TYGHRLUD]+?)\\]]+") %>%
     map(~tibble(str = if(length(.x[, 1]) == 0) "" else .x[, 1]) %>% mutate(
-      is_key = str_detect(str, "\\[([TYGHRLUD\\+]+?)\\]"),
+      is_key = str_detect(str, "\\[([TYGHRLUD]+?)\\]"),
       str = if_else(is_key,
-                    str_replace_all(str, "\\[([TYGHUDLR\\+]+?)\\]", "/img/\\1.svg") %>%
+                    str_replace_all(str, "\\[([TYGHUDLR]+?)\\]", file.path(dir, "\\1.svg")) %>%
                       # str_replace_all(str, "\\[([TYGHUDLR\\+]+?)\\]", "key-\\1") %>%
                       sapply(reuse_svg) %>%
                       unlist,
@@ -84,7 +113,7 @@ replace_keys <- function(text){
     ) %>% map_chr(~paste(.x$str, collapse = ", ") %>% str_remove("^,\\s"))
 }
 
-to_table <- function(d, lr = F){
+to_table <- function(d, lr = F, dir = "../../../../img/xbox"){
   t <- d %>% mutate(
     name = paste(name_orig, paste0("<br>(", name, ")")),
     mode = factor(mode, levels = c("normal", "long", "low", "high", "iron", "donna", "boartusk", "holy", "roof", "fool", "misc"))
@@ -96,9 +125,9 @@ to_table <- function(d, lr = F){
       lr = if_else(common, "l", lr)) %>%
       pivot_wider(id_cols = c(mode, name, common), names_from = lr, values_from = command) %>%
       mutate(l = if_else(is.na(l), "x", l), r = if_else(common, "", if_else(is.na(r), "x", r))) %>%
-      dplyr::select(-common) %>% mutate(across(c(l, r), replace_keys))
+      dplyr::select(-common) %>% mutate(across(c(l, r), replace_keys, dir = dir))
   } else{
-    t <- dplyr::select(t, -one_of("lr")) %>% mutate(command = replace_keys(command))
+    t <- dplyr::select(t, -one_of("lr")) %>% mutate(command = replace_keys(command, dir = dir))
   }
   t <- t %>% arrange(mode) %>% group_by(mode) %>% gt()
   # HTMLをエスケープする方法として tab_row_group を使うものがあるが, rownames が表示されたままになる
@@ -106,33 +135,40 @@ to_table <- function(d, lr = F){
   # よって内部データを改ざんするしかない
   g_headings <- list(
     normal = "通常時",
-    long = "ロングガード時 - [スペース] 押しっぱなし",
+    long = "ロングガード時 - スペース or LT 押しっぱなし",
     low = html(
-      sprintf("下段の構え時 - %s + [スペース] 押しっぱなし)",
-              paste(reuse_svg("key-D"), collapse = "\n"))
+      sprintf("下段の構え時 - %s + スペース or LT 押しっぱなし)",
+              paste(reuse_svg(file.path(dir, "D.svg")), collapse = "\n"))
     ),
     iron = html(
-      sprintf("『鉄の門』の構え時 - %s + [スペース] 押しっぱなし",
-              paste(readLines(here::here("img/D.svg")), collapse = "\n"))
+      sprintf("『鉄の門』の構え時 - %s + スペース or LT 押しっぱなし",
+              paste(reuse_svg(file.path(dir, "D.svg")), collapse = "\n")
+              )
     ),
     high = html(
-      sprintf("『上段』の構え時 (%s + [スペース] 押しっぱなし)",
-              paste(reuse_svg("key-U"), collapse = "\n"))
+      sprintf("『上段』の構え時 (%s + スペース or LT 押しっぱなし)",
+              paste(reuse_svg(file.path(dir, "U.svg")), collapse = "\n"))
     ),
     boartusk = html(
-      sprintf("『猪の牙』の構え時 (%s + 「スペース」押しっぱなし)",
-              paste(reuse_svg("key-U"), collapse = "\n"))
+      sprintf("『猪の牙』の構え時 (%s + スペース or LT 押しっぱなし)",
+              paste(reuse_svg(file.path(dir, "U.svg")), collapse = "\n"))
     ),
     donna = html(
       sprintf("『貴婦人』の構え時 (%s  %s %s)",
-              paste(reuse_svg("key-D"), collapse = "\n"),
-              paste(reuse_svg("key-L"), collapse = "\n"),
-              paste(reuse_svg("key-Y"), collapse = "\n")
+              paste(reuse_svg(file.path(dir, "D.svg")), collapse = "\n"),
+              paste(reuse_svg(file.path(dir, "L.svg")), collapse = "\n"),
+              paste(reuse_svg(file.path(dir, "Y.svg")), collapse = "\n")
       )
     ),
-    holy = html(sprintf("『聖なる』構え時 (%s)", paste(reuse_svg("key-D"), reuse_svg("key-R"), reuse_svg("key-Y"), collapse = "\n"))),
-    roof = html(sprintf("『屋根』の構え時 (%s + [スペース] 押しっぱなし)", reuse_svg("key-U"))),
-    fool = html(sprintf("『愚者』の構え時 (%s + [スペース] 押しっぱなし)", reuse_svg("key-D"))),
+    holy = html(
+      sprintf("『聖なる』構え時 (%s)",
+              paste(
+                reuse_svg(file.path(dir, "D.svg")),
+                reuse_svg(file.path(dir, "R.svg")),
+                reuse_svg(file.path(dir, "Y.svg")), collapse = "\n")
+              )),
+    roof = html(sprintf("『屋根』の構え時 (%s + スペース or LT 押しっぱなし)", reuse_svg(file.path(dir, "U.svg")))),
+    fool = html(sprintf("『愚者』の構え時 (%s + スペース or LT 押しっぱなし)", reuse_svg(file.path(dir, "D.svg")))),
     misc = html("共通コマンド")
   )
   t$`_stub_df`[["group_label"]] <- map(t$`_stub_df`[["group_label"]], ~html(g_headings[[.x]]))
